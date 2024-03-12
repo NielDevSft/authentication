f45e960d322ba4b7e46d0af88e30dd82a574611f
NielDevSft <85716027+NielDevSft@users.noreply.github.com>
2024-03-12 01:05:52 -0300

    Ajuste JwtClaim

---
+++ api/JWTAuthentication/JWTAuthentication.Persistence/Migrations/20240312040307_alter-table-jwtclaim.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JWTAuthentication.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class altertablejwtclaim : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClaimId",
+                schema: "app",
+                table: "RoleJwtClaim");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClaimId",
+                schema: "app",
+                table: "RoleJwtClaim",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
